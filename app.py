4908fc47952ce90e3d2280bee0b3614dd03a453f
6d6d <57275657+6d6d@users.noreply.github.com>
2022-08-01 19:37:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        @app.route('/video_start')
+@app.route('/video_start')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
